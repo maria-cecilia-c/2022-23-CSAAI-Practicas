303ac83f5d14734e3194f60930e207d814c1a8e3
MCcampos <mc.campos.2020@alumnos.urjc.es>
2023-03-18 10:10:04 +0100

    cambio hechos en mac

--- P2/BOOM/botones2 copy.js
+++ P2/BOOM/botones2 copy.js
@@ -3,18 +3,17 @@ var num_Secreto = [];
 
 aciertos =[]; 
 
+
 //CLAVE SECRETA SERÁ UN NUMERO ALEATORIA DE 4 DIGITOS 
 //hacer función para llamar
 function CrearNumSecreto(secreto){
     for (var i = 0; i < 4; i++) { //0-9    
-    
         num1 = Math.floor(Math.random()*10);
         secreto.push(num1);
-        console.log(num1);
     }
 }
-CrearNumSecreto(num_Secreto)
-console.log(num_Secreto)
+CrearNumSecreto(num_Secreto);
+console.log('NUMERO SECRETO',num_Secreto)
 
 //-- Elementos de la interfaz de la calculadora
 //-- Estados de la calculadora
@@ -24,7 +23,8 @@ const ESTADO = {
     OPERATION: 2,
     OP2: 3
 }
-//-- Elementos de la gui
+const click_sound = new Audio('smile.mp3');
+
 const gui = {
     display : document.getElementById("display"),
     start : document.getElementById("start"),
@@ -34,15 +34,16 @@ const gui = {
 
 //-- Definir un objeto cronómetro
 const crono = new Crono(gui.display);
+const cancion = document.getElementById("cancion")
 
- //-- Variable de estado de la calculadora
- //-- Al comenzar estamos en el estado inicial
- let estado = ESTADO.INIT;   
+//-- Variable de estado de la calculadora
+//-- Al comenzar estamos en el estado inicial
+let estado = ESTADO.INIT;   
 
 //-- Función de retrollamada de los digitos
 function digito(ev){
     if (estado == ESTADO.INIT) {
-        pantalla0.innerHTML = ev.target.value;
+        display.innerHTML = ev.target.value;
         estado = ESTADO.OP1;
     } else {
         //--En cualquier otro estado lo añadimos
@@ -58,28 +59,31 @@ for (let boton of digitos) {
     boton.onclick = (ev) => {
        console.log(ev.target.value) //par que no salga por pantalla lo que escribimos
         crono.start();
-        //tiene que cambiar en orden
-           //si tengo dos iguales se ponen los dos
+        
             for(j = 0; j < num_Secreto.length; j++)  {
 
                 if (num_Secreto[j] == ev.target.value){
                     pantalla0.innerHTML = num_Secreto[j] 
+                    pantalla0.style.cssText = 'color:  #49dbff;';
                     aciertos.push(num_Secreto[j] );
                     num_Secreto[j] = 11;
                 }
                 else if (num_Secreto[j+1] == ev.target.value){
                     pantalla1.innerHTML = num_Secreto[j+1]
+                    pantalla1.style.cssText = 'color:  #49dbff;';
                     aciertos.push(num_Secreto[j+1] );
                     num_Secreto[j+1]  = 11;
                 }
                 else if (num_Secreto[j+2] == ev.target.value){
                     pantalla2.innerHTML = num_Secreto[j+2] 
+                    pantalla2.style.cssText = 'color:  #49dbff;';
                     aciertos.push(num_Secreto[j+2] );
                     num_Secreto[j+2]  = 11;
                    
                 }
                 else if(num_Secreto[j+3] == ev.target.value){
                     pantalla3.innerHTML = num_Secreto[j+3] 
+                    pantalla3.style.cssText = 'color:  #49dbff;'; //cambiar el colos si acertamos
                     aciertos.push(num_Secreto[j+3] ); 
                     num_Secreto[j+3]  = 11;
                 }
@@ -89,8 +93,9 @@ for (let boton of digitos) {
             }  //función de recursividad
             if (num_Secreto.length==aciertos.length){
                 crono.stop();
+                num_Secreto=[];
             }
-  
+           
     }    
 }
 
@@ -109,6 +114,7 @@ gui.start.onclick = () => {
 gui.stop.onclick = () => {
     console.log("Stop!");
     crono.stop();
+    click_sound.pause();
 }
 
 //-- Reset del cronómetro
@@ -116,16 +122,31 @@ gui.reset.onclick = () => {
     console.log("Reset!");
     crono.reset();
 }
+cancion.onclick=() => {
+    click_sound.currentTime = 0;
+    click_sound.play();
+}
 
 
 //-- Poner a cero la expresion quitar despues
 reset.onclick = () => {
+    crono.stop();
+    crono.reset();
+    num_Secreto=[];
+    aciertos=[];
+    pantalla0.style.cssText = 'color:  #91ff00;';
+    pantalla1.style.cssText = 'color:  #91ff00;';
+    pantalla2.style.cssText = 'color:  #91ff00;';
+    pantalla3.style.cssText = 'color:  #91ff00;';
     display.innerHTML = "0:0:0";
+    estado = ESTADO.INIT;
     pantalla0.innerHTML = "*";
     pantalla1.innerHTML = "*";
     pantalla2.innerHTML = "*";
     pantalla3.innerHTML = "*";
-    }
+    CrearNumSecreto(num_Secreto);
+    console.log('NUMERO SECRETO',num_Secreto)
+}
     
 
 
