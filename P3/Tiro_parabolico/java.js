3c0b1bd5fb4cbc1eca4585dd3d79af07dba92744
MCcampos <mc.campos.2020@alumnos.urjc.es>
2023-03-23 10:45:51 +0100

    diapro

--- P3/Tiro_parabolico/java.js
+++ P3/Tiro_parabolico/java.js
@@ -9,10 +9,6 @@ canvas.height = 200;
 //-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
 
-//-- Coordenadas del objeto
-let x = 0;
-let y = 10;
-
 //-- Velocidad horizontal del objeto
 
 //-------------------DESLIZADORES------------------------------------
@@ -30,20 +26,42 @@ range2.oninput = () => {
 }
 
 //------------------------------
+//-- Coordenadas del objeto
+let x = 0;
+let y = 180;
 
-var velx = 0;
+//-- Velocidades del objeto
+var velx = velocidad.innerHTML;
+var vely = 0;
+
 //-- Función principal de animacion
 function update() 
 {
-  console.log("test");
-  x = x +  velocidad.innerHTML ;
-
-  //-- 2) Borrar el canvas
+    console.log("test");
+    //-- Algoritmo de animacion:
+    //-- 1) Actualizar posición del  elemento
+    //-- (física del movimiento rectilineo uniforme)
+  
+     //-- Condición de rebote en extremos verticales del canvas
+    if (x < 0 || x >= (canvas.width - 15) ) {
+    
+      velx = -velx;
+    }
+  
+    //-- Condición de rebote en extremos horizontales del canvas
+    if (y <= 0 || y > 80) {
+      vely = -vely;
+    }
+  
+    //-- Actualizar la posición
+    x = x + velx;
+    y = y + vely;
+  
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   //-- 3) Dibujar los elementos visibles
   ctx.beginPath();
-    ctx.rect(x, y, 20, 20);
+    ctx.rect(x, y, 20, 20); //tamaño del cuadrado 
 
     //-- Dibujar
     ctx.fillStyle = 'red';
@@ -53,7 +71,7 @@ function update()
 
     //-- Dibujar el trazo
     ctx.stroke()
-  ctx.closePath();
+    ctx.closePath();
 
   //-- 4) Volver a ejecutar update cuando toque
   requestAnimationFrame(update);
